8c030fbdc6b2a8627a60ddf61341670295fb9d12
tbruhnuncc <tbruhn@uncc.edu>
2024-10-03 17:09:56 -0400

    feat: Closes #6 -added mongoose schema for product.js -added mongoose schema for user.js

--- models/product.js
+++ models/product.js
@@ -1,8 +1,39 @@
-//title String
-//seller User relation
-//condition String
-//price number
-//description String
-//tags [String]
-//image
-//active boolean
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, "Product title is required"],
+  },
+  seller: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: [true, "Product seller is required"],
+  },
+  condition: {
+    type: String,
+    required: [true, "Product condition is required"],
+  },
+  price: {
+    type: Number,
+    required: [true, "Product price is required"],
+  },
+  description: {
+    type: String,
+    required: [true, "Product description is required"],
+  },
+  tags: {
+    type: [String],
+    required: [true, "Product tags are required"],
+  },
+  image: {
+    type: String,
+    required: [true, "Product image is required"],
+  },
+  active: {
+    type: Boolean,
+    default: true,
+  },
+});
+
+module.exports = mongoose.model("Product", productSchema);
