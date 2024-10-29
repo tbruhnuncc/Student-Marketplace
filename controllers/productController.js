ac416ecddc8d27b22a70985ad2168c0148ef7184
tbruhnuncc <tbruhn@uncc.edu>
2024-10-29 17:06:32 -0400

    Closes: #58 -userController: Updates serialization to store the user objectId in the session rather than the user information -productController: changes product.seller to be equal to the user object id stored in the session -product.js: changed product seller to be of type ObjectId with a ref to User model.

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,97 +1,98 @@
 const product = require("../models/product");
 const model = require("../models/product");
+const mongoose = require("mongoose");
 
 exports.read = (req, res, next) => {
-    let id = req.params.id;
-    model.findById(id)
-    .then(product=>{
-        if (product) {
-            res.send(product);
-        }
-        else {
-            next(err);
-        }
+  let id = req.params.id;
+  model
+    .findById(id)
+    .then((product) => {
+      if (product) {
+        res.send(product);
+      } else {
+        next(err);
+      }
     })
-    .catch(err=>next(err));
-}
+    .catch((err) => next(err));
+};
 
 exports.update = (req, res, next) => {
-    let id = req.params.id;
-    let part = req.body;
-    model.findByIdAndUpdate(id, part, {useFindAndModify: false, runValidators: true})
-    .then(part=>{
-        if (part) {
-            res.send(part);
-        }
-        else {
-            next(err);
-        }
+  let id = req.params.id;
+  let part = req.body;
+  model
+    .findByIdAndUpdate(id, part, {
+      useFindAndModify: false,
+      runValidators: true,
     })
-    .catch((err)=>{
+    .then((part) => {
+      if (part) {
+        res.send(part);
+      } else {
         next(err);
+      }
+    })
+    .catch((err) => {
+      next(err);
     });
-}
+};
 
 exports.delete = (req, res, next) => {
-    let id = req.params.id;
-    model.findByIdAndDelete(id)
-    .then(result=>{
-        if (result) {
-            res.send(result);
-        }
-        else {
-            next(err);
-        }
+  let id = req.params.id;
+  model
+    .findByIdAndDelete(id)
+    .then((result) => {
+      if (result) {
+        res.send(result);
+      } else {
+        next(err);
+      }
     })
-    .catch(err=>next(err));
-}
+    .catch((err) => next(err));
+};
 
 exports.index = (req, res, next) => {
-    model.find()
-    .then(products=>{
-        if (products) {
-            res.render("./product/index", {products});
-        }
-        else {
-            next(err);
-        }
+  model
+    .find()
+    .then((products) => {
+      if (products) {
+        res.render("./product/index", { products });
+      } else {
+        next(err);
+      }
     })
-    .catch(err=>next(err));
-}
+    .catch((err) => next(err));
+};
 
 exports.show = (req, res, next) => {
-    let id = req.params.id;
-    model.findById(id)
-    .then(product=>{
-        if (product) {
-            res.render("./product/show", {product});
-        }
-        else {
-            next(err);
-        }
+  let id = req.params.id;
+  model
+    .findById(id)
+    .then((product) => {
+      if (product) {
+        res.render("./product/show", { product });
+      } else {
+        next(err);
+      }
     })
-    .catch(err=>next(err));
-}
+    .catch((err) => next(err));
+};
 
 exports.new = (req, res, next) => {
-    res.render("./product/new");
-}
+  res.render("./product/new");
+};
 
 exports.create = (req, res, next) => {
-    console.log(req.body);
-    let product = new model(req.body);
-    product.seller = req.session.passport.user.firstName + " " + req.session.passport.user.lastName;
-    product.image = "/images/" + req.file.filename;
-    product.active = true;
-    product.tags = ["placeholder1", "placeholder2"];
-    product.save()
-    .then(result => {
-        res.redirect("/products");
+  let product = new model(req.body);
+  product.seller = req.session.passport.user.id;
+  product.image = "/images/" + req.file.filename;
+  product.active = true;
+  product.tags = ["placeholder1", "placeholder2"];
+  product
+    .save()
+    .then((result) => {
+      res.redirect("/products");
     })
-    .catch(err => {
-        next(err);
+    .catch((err) => {
+      next(err);
     });
-}
-
-
-    
+};
