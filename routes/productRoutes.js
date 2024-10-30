d427a6c1d719d8a2e8037646e89d1ffdd6d65351
tbruhnuncc <tbruhn@uncc.edu>
2024-10-29 22:14:03 -0400

    Closes: #62 -Moved file upload multer middleware to a seperate middleware file in the middleware file -Also edited new.ejs to fix fileupload errors from merging -removed redundent require in auth.js

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,32 +1,9 @@
 const express = require('express');
 const controller = require('../controllers/productController');
 const { isLoggedIn, isGuest } = require("../middleware/auth");
+const upload = require('../middleware/fileUpload');
 
 const router = express.Router();
-
-const multer = require('multer');
-const path = require('path');
-
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, './public/images');
-      },
-      filename: function (req, file, cb) {
-        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-        cb(null, uniqueSuffix + path.extname(file.originalname))
-      }
-})
-
-const fileFilter = (req, file, cb) => {
-    const mimeTypes = ['image/jpeg', 'image/png', 'image/gif'];
-    if(mimeTypes.includes(file.mimetype)) {
-        return cb(null, true);
-    } else {
-        cb(new Error('Invalid file type. Only jpg, jpeg, png, and gif image files are allowed'), false);
-    }
-}
-
-const upload = multer({storage, fileFilter, limits: {fileSize: 2*1024_1024}}).single('image');
 
 //GET /products: send all products
 router.get('/', controller.index)
